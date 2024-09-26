a95946bbb6b0b57c49508e2c70916978d7742597
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-09-26 12:44:08 +0500

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// int primeNum(int n)
+// {
+
+// }
+// int main()
+// {
+//   int n;
+//   cout<<"Enter a number : ";
+//   cin>>n;
+//   bool flag=true;
+  
+
+//   for(int i=2;i<n;i++)
+//   {
+   
+//     if(n%i==0)
+//     {
+//       flag=false;
+//       break;
+//     }
+//   }
+//     // Output the result
+//     if (flag==true) {
+//         cout << n << " is a prime number." << endl;
+//     } else {
+//         cout << n << " is not a prime number." << endl;
+//     }
+// }
