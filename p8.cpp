7e72e29f6d50c0680139907a38be22ce72e0d905
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-10-05 19:16:24 +0500

    Add files via upload

---
+++ p8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void printHello()
+{
+  cout<<"Hello World";
+}
+int main()
+{
+  printHello();
+
+}
---
+++ p9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int printSum(int a, int b)
+{
+  int sum = a + b;
+  cout<<"The sum is equal to : "<<sum;
+  
+}
+
+int main()
+{
+  printSum(4,9);
+
+}
